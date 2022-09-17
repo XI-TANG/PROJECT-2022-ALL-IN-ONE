6e7d4f40a1ee8feeaf39c10ad337163311e9f31a
XI-TANG <mobai2209122642@gmail.com>
2022-09-17 23:27:41 +0800

    Update main.cpp

--- 00-OpenGL-Learn/01-OpenGL-Project/LearnOpenGL-CN/OpenGL-2-triangle/triangle/src/main.cpp
+++ 00-OpenGL-Learn/01-OpenGL-Project/LearnOpenGL-CN/OpenGL-2-triangle/triangle/src/main.cpp
@@ -39,7 +39,12 @@ int main() {
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
-	unsigned int VBOId = 0;
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+	unsigned int VBOID = 0;
 	/*
 	* 使用glGenBuffers后，它就会把buffer memory的ID保存在VBOId里
 	* 我们使用的是&VBOId，也就是glGenBuffers会修改VBOId的值
@@ -49,9 +54,32 @@ int main() {
 	* 换句话说，这个名称虽然是GLuint类型的，但是你万万不能直接指定个常量比如说0
 	* 即，同一时间，不能绑定两个相同类型的缓冲对象
 	*/
-	glGenBuffers(1,&VBOId);
+	glGenBuffers(1,&VBOID);
 	//printf("glGenBuffers ID: %d\n", VBOId);
+	/*
+	* glBindBuffer将绑定最近的VBO
+	* GL_ARRAY_BUFFER与GL_ELEMENT_ARRAY_BUFFER是常用的参数
+	* the buffer type of a vertex buffer object is GL_ARRAY_BUFFER
+	* GL_ARRAY_BUFFER用于VBO和VAO
+	* GL_ELEMENT_ARRAY_BUFFER用于EBO
+	*/
+	glBindBuffer(GL_ARRAY_BUFFER,VBOID);
+	/*
+	* glBufferData is a function specifically targeted to copy user-defined data into the currently bound buffer
+	* 第一个参数target：the type of the buffer we want to copy data into: the vertex buffer object currently bound to the GL_ARRAY_BUFFER target.
+	* 第二个参数size：buffer(data) size
+	* 第三个参数*data：data指针用于传入数据
+	* 第四个参数：The fourth parameter specifies how we want the graphics card to manage the given data
+	*		GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+	*		GL_STATIC_DRAW: the data is set only once and used many times.
+	*		GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
 
+	/*
+	* 我们通过glBufferData将数据传入GPU memory里面了，接下来需要使用shader（渲染处理程序）来处理数据
+	* 我们需要的shader通常只需要两个：vertex shader和fragment shader
+	*/
 
 	while (!glfwWindowShouldClose(window)) {
 		
